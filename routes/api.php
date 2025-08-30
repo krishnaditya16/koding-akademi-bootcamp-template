c274760ce14a2e7f9cad0fd720340139b9bc5e08
Krishna Aditya <krishna.aditya125@gmail.com>
2025-08-30 09:30:28 +0800

    feat: add cors config

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,11 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
-});
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::post('register', [AuthController::class, 'register'])->name('auth.register');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('auth.logout');
     Route::get('profile', [AuthController::class, 'profile'])->name('auth.profile');
     Route::post('order', [OrderController::class, 'createOrder'])->name('order.create');
     Route::post('order/{order}/pay', [OrderController::class, 'payOrder'])->name('order.pay');
